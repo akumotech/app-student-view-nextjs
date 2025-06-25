a26e262f7627a90f63eb5e360c2d0c70821a8310
abdul.sharif <abdugofir00@gmail.com>
2025-06-25 11:02:40 -0500

    callback AuthProvder wrap

--- app/callback/layout.tsx
+++ app/callback/layout.tsx
@@ -1,9 +1,17 @@
 import { Suspense } from "react";
+import { AuthProvider } from "@/lib/auth-context";
+import { getUserServer } from "@/lib/getUserServer";
 
-export default function layout({
+export default async function CallbackLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <Suspense>{children}</Suspense>;
+  const { user, isAuthenticated } = await getUserServer();
+
+  return (
+    <AuthProvider initialUser={user} initialIsAuthenticated={isAuthenticated}>
+      <Suspense>{children}</Suspense>
+    </AuthProvider>
+  );
 }
